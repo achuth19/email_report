e5ec9d408def22e688448c1f4077c32e2556edf8
achuth19 <85541360+achuth19@users.noreply.github.com>
2023-08-14 17:41:35 +0530

    Update settings.py
    allowing host changed

--- tcubesa/settings.py
+++ tcubesa/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 # from celery.schedules import crontab
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hm#q-qw^rwf+0ib^de8nkmph=+_^vp)y51_x^r4*3nxnf22fja
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["44.202.67.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
